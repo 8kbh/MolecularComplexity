37793993ef6b3fc69b5816b2da54c90195b36581
Oleg K <13.meilleur@gmail.com>
2025-11-01 15:43:10 +0300

    remove unnecesary scripts

--- scripts/server.py
+++ scripts/server.py
@@ -5,9 +5,31 @@ from flask_cors import CORS
 import pandas as pd
 from rdkit import Chem, RDLogger
 from tqdm import tqdm
-from utils import get_predictor
 import logging
 import random
+import pickle
+from mc.analyzers import Predictor
+
+
+def load_file(path: str, type="pickle"):
+    if type == "pickle":
+        with open(path, "rb") as f:
+            data = pickle.load(f)
+    elif type == "json":
+        with open(path, "r") as f:
+            data = json.load(f)
+    elif type == "csv":
+        data = pd.read_csv(path)
+    else:
+        raise ValueError("Type not supported")
+    return data
+
+
+def get_predictor() -> Predictor:
+    dump = load_file("data/model.pkl", "pickle")[(0.7, 0.1, 0.2)]
+    ranker = dump["ranker"]
+    scaler = dump["scaler"]
+    return Predictor(ranker, scaler)
 
 
 RDLogger.DisableLog('rdApp.*')
